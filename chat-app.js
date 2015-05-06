90b94f4701a4262170a043eaf2e299964bcdeff8
Sergiy Dybskiy <sdybskiy@gmail.com>
2015-05-06 15:45:10 -0400

    deleted insecure and autopublish and set up router

--- chat-app.js
+++ chat-app.js
@@ -3,6 +3,25 @@ Rooms = new Meteor.Collection("rooms");
 
 if (Meteor.isClient) {
   // client code
+  Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_ONLY'
+  });
+
+  Template.welcome.rendered = function() {
+    Tracker.autorun(function(){
+      if (Meteor.userId()) {
+        Router.go('lobby');
+      }
+    });
+  }
+
+  Template.lobby.rendered = function() {
+    Tracker.autorun(function(){
+      if (!Meteor.userId()) {
+        Router.go('/');
+      }
+    });
+  }
 }
 
 if (Meteor.isServer) {
