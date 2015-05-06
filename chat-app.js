e13cf00d59bd279614b0a20d4ae643c539ec6b72
Sergiy Dybskiy <sdybskiy@gmail.com>
2015-05-06 15:12:38 -0400

    added collections for rooms and messages

--- chat-app.js
+++ chat-app.js
@@ -1,23 +1,10 @@
+Messages = new Meteor.Collection("messages");
+Rooms = new Meteor.Collection("rooms");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+  // client code
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+  // server code
 }
